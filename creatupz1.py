2062e142bc57f9175860186f282cb4f0e0ea1e3d
juanvgus <79470139+juanvgus@users.noreply.github.com>
2023-09-06 14:46:43 -0500

    Update creatupz1.py

--- creatupz1.py
+++ creatupz1.py
@@ -21,19 +21,21 @@ target_key = "dataset0.csv"
 geolocator = Nominatim(user_agent="my_app")
 
 def crear():
-  response = s3_client.get_object(Bucket=source_bucket, Key=source_key)
-  content = response['Body'].read()
-  #table = pq.read_table(io.BytesIO(content))
-  #df = table.to_pandas()
-  df = pd.read_parquet(content)
-  upz = np.array([upz(lat, lon) for lat, lon in zip(df['LATITUDEORI'], df['LONGITUDEORI'])], dtype=object)
-  upz = pd.DataFrame({'UPZ': upz})
-  df = pd.concat([df, upz], axis=1)
-  modified_table = pq.Table.from_pandas(df)
-  output_stream = io.BytesIO()
-  pq.write_table(modified_table, output_stream)
-  s3_client.put_object(Bucket=target_bucket, Key=target_key, Body=output_stream.getvalue())
-  return {
+    response = s3_client.get_object(Bucket=source_bucket, Key=source_key)
+    content = response['Body'].read()
+
+    df = pd.read_csv(io.BytesIO(content))
+
+    upz = np.array([upz(lat, lon) for lat, lon in zip(df['LATITUDEORI'], df['LONGITUDEORI'])], dtype=object)
+    upz = pd.DataFrame({'UPZ': upz})
+
+    df = pd.concat([df, upz], axis=1)
+
+    df.to_csv('mi_archivo.csv', index=False)
+
+    s3_client.put_object(Bucket=target_bucket, Key=target_key, Body=open('mi_archivo.csv', 'rb'))
+
+    return {
         'statusCode': 200,
         'body': json.dumps('Proceso completado exitosamente.')
     }
